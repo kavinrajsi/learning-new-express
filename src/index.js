c19bae93d90c6262151138644e6c295dd6f8840b
kavin raj <we@sikavinraj.me>
2020-02-02 11:46:20 +0530

    get and post

--- src/index.js
+++ src/index.js
@@ -1,9 +1,31 @@
-var http = require("http");
+const express = require("express");
+const student2 = require("./module/students");
+const bodyParser = require("body-parser");
 
-//create a server object:
-http
-  .createServer(function(req, res) {
-    res.write("Hello World!"); //write a response to the client
-    res.end(); //end the response
-  })
-  .listen(8080); //the server object listens on port 8080
+const app = express();
+
+// middleware
+// app.use((req, res, next) => {
+//   // res.send("demo from middlware");
+//   next();
+// });
+
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
+app.get("/students", (req, res) => {
+  // res.send(student2);
+  res.status(200).json({ student2 });
+});
+
+app.post("/students", (req, res) => {
+  res.send("post data for response");
+  console.log(req.body);
+});
+
+const server = app.listen(8080, () => {
+  console.log(`Server running in port ${server.address().port}`);
+});
